5cbe1816e2336a8c34474d6cfd15127bd0abf463
Ian Roberts <ianr9532@gmail.com>
2025-03-26 20:18:16 -0400

    Updated index and created package

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
+const http = require("http");
+
 console.log("Node.js in Jenkins Pipeline Please!");
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Basic Index Page</title>
+            </head>
+            <body>
+                    <h1>Welcome to the Basic Index Page</h1>
+                    <p>This is a simple HTML page served with Node.js.</p>
+            </body>
+            </html>
+        `);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
 // const http = require("http");
 
 // const server = http.createServer((req, res) => {
